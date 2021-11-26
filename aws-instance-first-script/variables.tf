97a7b290b8a992758ba2284338c13b057182acd7
sk4evercache <95042038+sk4evercache@users.noreply.github.com>
2021-11-26 17:39:18 +0530

    amid

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -3,11 +3,5 @@ variable "region" {
 }
 
 variable "ami_id" {
-  type = "map"
-
-  default = {
-    eu-west-1    = "ami-0d1bf5b68307103c2"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
-  }
+  default =  "ami-0d1bf5b68307103c2"
 }
