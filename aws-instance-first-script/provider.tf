5dee596bb3189188bc7b00ee252dec403e1b3b31
sk4evercache <95042038+sk4evercache@users.noreply.github.com>
2021-11-26 17:43:51 +0530

    pro update

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,5 +1,17 @@
-provider "aws" {
-  required_version = ">= 0.12.14"
-  region     = "eu-west-1"
-  }
 
+ terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.67.0"
+    }
+  }
+   
+ provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
