19188c1688f0704914edded0c056ad4548d515fd
sk4evercache <95042038+sk4evercache@users.noreply.github.com>
2021-11-26 16:54:31 +0530

    region changes

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,12 +1,12 @@
 variable "region" {
-  default = "us-east-1"
+  default = "eu-west-1"
 }
 
 variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
+    eu-west-1    = "ami-0d1bf5b68307103c2"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
     eu-central-1 = "ami-9787h5h6nsn"
   }
